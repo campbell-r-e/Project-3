9bc878cc312f0faba6e1a51f221ac944780b796b
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-19 21:55:29 -0400

    Intial

---
+++ TCPFileClient.java
@@ -0,0 +1,52 @@
+package tcp;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+public class TCPFileClient {
+    public static void main(String[] args) throws  Exception {
+        if (args.length != 2) {
+            System.out.println();
+            return;
+        }
+        int serverPort = Integer.parseInt(args[1]);
+        String command;
+        do {
+            Scanner keyboard = new Scanner(System.in);
+            command = keyboard.nextLine().toUpperCase();
+
+            switch (command) {
+                case "D"://delete
+                    String fileName = keyboard.nextLine();
+                    ByteBuffer request = ByteBuffer.wrap((command + fileName).getBytes());
+                    SocketChannel channe = SocketChannel.open();
+                    channe.connect(new InetSocketAddress(args[0], serverPort));
+                    channe.write(request);
+                    channe.shutdownOutput();
+                     // todo: receive server code and tell the user.
+
+                    break;
+                case "L"://list
+                    break;
+                case "R": // rename
+                    break;
+
+                case "U": // upload
+                    break;
+                case "G": // Download
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }// end of switch statement
+
+        }
+        while (!command.equals("Q"));{
+
+
+        }
+
+
+    }
+}
---
+++ server.java
@@ -0,0 +1,71 @@
+package tcp;
+
+import java.io.File;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class server {
+    public static void main(String[] args) throws  Exception{
+        ServerSocketChannel Listenchannel= ServerSocketChannel.open();
+        Listenchannel.bind(new InetSocketAddress(3000));
+        while(true) {
+            // to keep server
+            SocketChannel serveChannel = Listenchannel.accept();
+            ByteBuffer request = ByteBuffer.allocate(1024);
+            int numBytes = serveChannel.read(request);
+            request.flip();
+
+            // Size of byte array should match number bytes for command
+
+            byte[] a = new byte[1];
+            request.get(a);
+            String command = new String(a);
+            System.out.println("\n recieved command:"+command);
+
+            switch (command){
+                case "D":
+                    byte[]d= new byte[request.remaining()];
+                    request.get(d);
+                    String Filename = new String(d);
+                    System.out.println("File to delte"+Filename);
+                    File file = new File("ServerFiles/"+Filename);
+                    Boolean sucess = false;
+                    if(file.exists()){
+                        sucess = file.delete();
+
+
+                    }
+                    String replymessage;
+                    if(sucess){
+                        replymessage = "s";
+
+                    }
+                    else{
+                        replymessage = "f";
+
+                    }
+                    ByteBuffer reply = ByteBuffer.wrap(replymessage.getBytes());
+                    serveChannel.write(reply);
+                    serveChannel.close();
+
+
+
+                    break;
+                case "L":
+                    break;
+                case "R":
+                    break;
+                case "U":
+                    break;
+                case "G":
+                    break;
+                default:
+                    System.out.println("Invalid Command");
+            }
+
+        }
+
+    }
+}
