d84fbca20cce285f4d274963437870a379c1aaa0
Mythical-traveler <122392397+Mythical-traveler@users.noreply.github.com>
2024-09-22 20:35:19 -0400

    Implemented Upload and Download Functions

--- tcpFileService/TCPFileClient.java
+++ tcpFileService/TCPFileClient.java
@@ -1,5 +1,8 @@
 package tcpFileService;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
@@ -39,7 +42,7 @@ public class TCPFileClient {
                         System.out.println("File successfully Deleted");
 
                     } else if (code.equals("F")) {
-                        System.out.println("Failde to delete file");
+                        System.out.println("Failed to delete file");
 
                     }
                     else{
@@ -49,13 +52,94 @@ public class TCPFileClient {
 
                     break;
                 case "L"://list
+
                     break;
                 case "R": // rename
                     break;
 
                 case "U": // upload
+                    System.out.println("Enter File Name: ");
+                    String fileToUpload = keyboard.nextLine();
+                    File file = new File(fileToUpload);
+
+                    // Check if file exists
+                    if (!file.exists()) {
+                        System.out.println("File does not exist.");
+                        break;
+                    }
+
+                    // Prepare the command and file size header
+                    SocketChannel uploadChannel = SocketChannel.open();
+                    uploadChannel.connect(new InetSocketAddress(args[0], serverPort));
+                    FileInputStream fis = new FileInputStream(file);
+                    ByteBuffer header = ByteBuffer.wrap(("U" + fileToUpload).getBytes());
+                    uploadChannel.write(header);
+
+                    // Read and send file bytes
+                    byte[] buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = fis.read(buffer)) > 0) {
+                        ByteBuffer fileData = ByteBuffer.wrap(buffer, 0, bytesRead);
+                        uploadChannel.write(fileData);
+                    }
+                    fis.close();
+                    uploadChannel.shutdownOutput();
+
+                    // Wait for server response
+                    ByteBuffer uploadReply = ByteBuffer.allocate(1);
+                    uploadChannel.read(uploadReply);
+                    uploadChannel.close();
+                    uploadReply.flip();
+                    byte[] uploadResponse = new byte[1];
+                    uploadReply.get(uploadResponse);
+                    String uploadCode = new String(uploadResponse);
+
+                    if (uploadCode.equals("S")) {
+                        System.out.println("File uploaded successfully");
+                    } else if (uploadCode.equals("F")) {
+                        System.out.println("File upload failed");
+                    } else {
+                        System.out.println("Invalid server code received");
+                    }
                     break;
                 case "G": // Download
+                    System.out.println("Enter the file name to download: ");
+                    String fileToDownload = keyboard.nextLine();
+                    SocketChannel downloadChannel = SocketChannel.open();
+                    downloadChannel.connect(new InetSocketAddress(args[0], serverPort));
+
+                    // Send the command and file name to the server
+                    ByteBuffer downloadRequest = ByteBuffer.wrap(("G" + fileToDownload).getBytes());
+                    downloadChannel.write(downloadRequest);
+                    downloadChannel.shutdownOutput();
+
+                    // Prepare to receive the file or error response
+                    ByteBuffer downloadReply = ByteBuffer.allocate(1);
+                    downloadChannel.read(downloadReply);
+                    downloadReply.flip();
+                    byte[] downloadResponse = new byte[1];
+                    downloadReply.get(downloadResponse);
+                    String downloadCode = new String(downloadResponse);
+
+                    if (downloadCode.equals("S")) {
+                        System.out.println("Receiving file...");
+                        FileOutputStream fos = new FileOutputStream("downloaded_" + fileToDownload);
+
+                        ByteBuffer fileBuffer = ByteBuffer.allocate(1024);
+                        while (downloadChannel.read(fileBuffer) > 0) {
+                            fileBuffer.flip();
+                            fos.write(fileBuffer.array(), 0, fileBuffer.remaining());
+                            fileBuffer.clear();
+                        }
+                        fos.close();
+                        System.out.println("File download successful");
+                    } else if (downloadCode.equals("F")) {
+                        System.out.println("File not found on server.");
+                    } else {
+                        System.out.println("Invalid server code received");
+                    }
+
+                    downloadChannel.close();
                     break;
                 default:
                     if(!command.equals("Q")){
